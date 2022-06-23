5a44e7b3fbca5be8fa37dca3306a75aa1b6ab39a
carolinabezares <201353@ids.upchiapas.edu.mx>
2022-06-23 18:58:03 -0500

    Añadiendo la estructura de la vista de la descripcion de la receta

--- lib/src/page/primerpantalla.dart
+++ lib/src/page/primerpantalla.dart
@@ -3,16 +3,34 @@ import 'package:flutter/material.dart';
 class PrimerPantalla extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-      title: Text("Primer Pantalla"),
-      actions: [
-        TextButton(
-            onPressed: () {
-              Navigator.pushNamed(context, "contador");
-            },
-            child: Icon(Icons.arrow_forward, color: Colors.black)),
-      ],
-    ));
+    var width = MediaQuery.of(context).size.width;
+    var height = MediaQuery.of(context).size.height;
+    return SafeArea(
+      child: Scaffold(
+        body: Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.only(right: width * 0.5),
+              child: const Icon(Icons.arrow_back),
+            ),
+            Center(
+              child: SizedBox(
+                width: width * 0.7,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(50.0),
+                  child: Image.network(
+                    'https://th.bing.com/th/id/R.f018ea9971d348290cff64b28ddb2fe1?rik=kKNKsxFrlwmpzw&riu=http%3a%2f%2fjustaboutbaked.com%2fwp-content%2fuploads%2f2015%2f05%2fReese4-e1431290031817.jpg&ehk=ZAfd4c362WYA08bCiheN%2bnLzLm0CsSvcGEWIITtsmgw%3d&risl=&pid=ImgRaw&r=0',
+                  ),
+                ),
+              ),
+            ),
+            Text(
+              'Titulo receta',
+              style: TextStyle(fontSize: 20),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
