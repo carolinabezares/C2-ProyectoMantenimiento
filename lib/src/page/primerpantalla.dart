5c29a8e121de7889d55510c6ec4ab731ee28831e
carolinabezares <201353@ids.upchiapas.edu.mx>
2022-06-23 19:38:28 -0500

    Agregando variables a los textos y imagenes

--- lib/src/page/primerpantalla.dart
+++ lib/src/page/primerpantalla.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
 
 class PrimerPantalla extends StatelessWidget {
+  String recipeName = 'Titulo de receta';
+  static const String ingredientsTittle = 'Ingredientes';
+  static const String preparationTittle = 'Preparacion';
+  String ingredients = 'Ingredientes de la receta';
+  String preparation = 'Preparacion de la receta';
+  String imageLink =
+      'https://th.bing.com/th/id/R.f018ea9971d348290cff64b28ddb2fe1?rik=kKNKsxFrlwmpzw&riu=http%3a%2f%2fjustaboutbaked.com%2fwp-content%2fuploads%2f2015%2f05%2fReese4-e1431290031817.jpg&ehk=ZAfd4c362WYA08bCiheN%2bnLzLm0CsSvcGEWIITtsmgw%3d&risl=&pid=ImgRaw&r=0';
   @override
   Widget build(BuildContext context) {
     var width = MediaQuery.of(context).size.width;
@@ -23,22 +30,20 @@ class PrimerPantalla extends StatelessWidget {
                   width: width * 0.7,
                   child: ClipRRect(
                     borderRadius: BorderRadius.circular(50.0),
-                    child: Image.network(
-                      'https://th.bing.com/th/id/R.f018ea9971d348290cff64b28ddb2fe1?rik=kKNKsxFrlwmpzw&riu=http%3a%2f%2fjustaboutbaked.com%2fwp-content%2fuploads%2f2015%2f05%2fReese4-e1431290031817.jpg&ehk=ZAfd4c362WYA08bCiheN%2bnLzLm0CsSvcGEWIITtsmgw%3d&risl=&pid=ImgRaw&r=0',
-                    ),
+                    child: Image.network(imageLink),
                   ),
                 ),
               ),
-              const Text(
-                'Titulo receta',
+              Text(
+                recipeName,
                 style: TextStyle(fontSize: 23, height: 2.5),
               ),
               const Text(
-                'Ingredientes',
+                ingredientsTittle,
                 style: TextStyle(fontSize: 17, height: 4),
               ),
               const Text(
-                'Preparación',
+                preparationTittle,
                 style: TextStyle(fontSize: 17, height: 4),
               )
             ],
