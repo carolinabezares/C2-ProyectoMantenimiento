cbb0b2566c061c98b45c2682c4097f1c6872503b
carolinabezares <201353@ids.upchiapas.edu.mx>
2022-06-25 19:12:56 -0500

    RES-1: Crecaión de las c

---
+++ lib/src/Home/UI/Screen/home_view.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class HomeView extends StatefulWidget {
+  HomeView({Key? key}) : super(key: key);
+
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Column(
+          children: [
+            Row(
+              children: [
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.arrow_back),
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
