79464e281c8b0807187eb7a8d13646b0500784da
carolinabezares <201353@ids.upchiapas.edu.mx>
2022-06-26 20:43:43 -0500

    RES-1: Agregar las diviciones de cada respectiva receta en los apartados

--- lib/src/Home/UI/Screen/home_view.dart
+++ lib/src/Home/UI/Screen/home_view.dart
@@ -7,22 +7,35 @@ class HomeView extends StatefulWidget {
   State<HomeView> createState() => _HomeViewState();
 }
 
-class _HomeViewState extends State<HomeView> {
+class _HomeViewState extends State<HomeView> with TickerProviderStateMixin {
   final PageController controller = PageController(initialPage: 0);
-  final List<String> listBarOptions = ['Desayuno', 'Comida', 'Cena'];
-  final List<Tab> listTabOptions = [
-    Tab(
-      text: 'Desayuno',
-    ),
-    Tab(
-      text: 'Comida',
-    ),
-    Tab(
-      text: 'Cena',
-    ),
+  final List<String> listBarOptions = [
+    'Desayuno',
+    'Comida',
+    'Merienda',
+    'Cena',
+    'Postres',
   ];
+
+  final List<String> listRecipeOptions = [
+    'XXXX',
+    'XXXX',
+    'XXXX',
+    'XXXX',
+    'XXXX',
+    'XXXX',
+    'XXXX',
+    'XXXX',
+  ];
+  final List<String> listRecipeImages = [
+    'http://del.h-cdn.co/assets/16/04/3200x1800/hd-aspect-1453934758-chicken-bacon-spaghetti-delish.jpg'
+  ];
+  final List<Tab> listTabOptions = [];
+  final List<ListView> listCardOptions = [];
+
   int count = 0;
   int actual = 0;
+
   @override
   void initState() {
     super.initState();
@@ -34,6 +47,65 @@ class _HomeViewState extends State<HomeView> {
         });
       }
     });
+    setState(() {
+      for (var element in listBarOptions) {
+        listTabOptions.add(
+          Tab(
+            child: Text(
+              element,
+              style: TextStyle(color: Colors.black),
+            ),
+          ),
+        );
+      }
+    });
+    setState(() {
+      for (var i = 0; i < listTabOptions.length; i++) {
+        listCardOptions.add(
+          ListView(
+            children: getCards(),
+          ),
+        );
+      }
+    });
+  }
+
+  List<Card> getCards() {
+    List<Card> cardsRecipe = [];
+    for (var element in listRecipeOptions) {
+      cardsRecipe.add(
+        Card(
+          // Con esta propiedad modificamos la forma de nuestro card
+          // Aqui utilizo RoundedRectangleBorder para proporcionarle esquinas circulares al Card
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+
+          // Con esta propiedad agregamos margen a nuestro Card
+          // El margen es la separación entre widgets o entre los bordes del widget padre e hijo
+          margin: EdgeInsets.all(15),
+
+          // Con esta propiedad agregamos elevación a nuestro card
+          // La sombra que tiene el Card aumentará
+          elevation: 10,
+
+          // La propiedad child anida un widget en su interior
+          // Usamos columna para ordenar un ListTile y una fila con botones
+          child: Row(
+            children: <Widget>[
+              // Usamos ListTile para ordenar la información del card como titulo, subtitulo e icono
+              Flexible(
+                child: Text(element),
+              ),
+              SizedBox(
+                  width: 100,
+                  height: 100,
+                  child: Image.network(listRecipeImages[0])),
+            ],
+          ),
+        ),
+      );
+    }
+    return cardsRecipe;
   }
 
   void getOption() async {
@@ -44,56 +116,55 @@ class _HomeViewState extends State<HomeView> {
 
   @override
   Widget build(BuildContext context) {
+    var width = MediaQuery.of(context).size.width;
+    var height = MediaQuery.of(context).size.height;
+    TabController tabController = TabController(length: count, vsync: this);
     return SafeArea(
-      child: DefaultTabController(
-        length: count,
-        child: Scaffold(
-          body: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  IconButton(
-                    onPressed: () {},
-                    icon: Icon(Icons.arrow_back),
-                  ),
-                  Text("Recetas"),
-                  IconButton(
-                    onPressed: () {},
-                    icon: Icon(Icons.search),
-                  )
-                ],
+      child: Scaffold(
+        body: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.arrow_back),
+                ),
+                Text("Recetas"),
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.search),
+                )
+              ],
+            ),
+            Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
               ),
-              Container(
-                height: 45,
+              elevation: 5,
+              child: Container(
                 decoration: BoxDecoration(
-                    color: Colors.grey[300],
-                    borderRadius: BorderRadius.circular(25.0)),
+                  color: Colors.black.withOpacity(0.1),
+                  borderRadius: BorderRadius.circular(10),
+                ),
                 child: TabBar(
                   indicator: BoxDecoration(
-                      color: Colors.green[300],
-                      borderRadius: BorderRadius.circular(25.0)),
-                  labelColor: Colors.white,
-                  unselectedLabelColor: Colors.black,
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.orange),
+                  controller: tabController,
+                  isScrollable: true,
+                  labelPadding: EdgeInsets.symmetric(horizontal: 30),
                   tabs: listTabOptions,
                 ),
               ),
-              const Expanded(
-                  child: TabBarView(
-                children: [
-                  Center(
-                    child: Text("Chats Pages"),
-                  ),
-                  Center(
-                    child: Text("Status Pages"),
-                  ),
-                  Center(
-                    child: Text('Calls Page'),
-                  ),
-                ],
-              ))
-            ],
-          ),
+            ),
+            Expanded(
+              child: TabBarView(
+                controller: tabController,
+                children: listCardOptions,
+              ),
+            ),
+          ],
         ),
       ),
     );
